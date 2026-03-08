e1bfb0c2d21a29db2f57b9725d9da9df2a2242bf
Sujan Adhikari <sujan69834@gmail.com>
2026-03-08 21:31:41 +0545

    Pandas 5

---
+++ adding_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    "Name": ["Chirs", "Steve", "Robert", "Tony", "Mark", "Natasha", "Jeremy", "Tom", "Teddy", "Marshall" ],
+    "Age": [24, 58, 45, 55, 22, 46, 67, 56, 43, 56],
+    "Salary": [50000, 45000, 43000, 56000, 45000, 50000, 34000, 54000, 60000, 45000],
+    "Rating": [4.5, 4.0, 3.9, 4.6, 4.7, 4.1, 4.3, 4.1, 4.33, 4.19]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+df["Bonus"] = df["Salary"]*0.2
+print(df)
+
